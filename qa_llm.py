4e5ae74f14b4826f2120781eba64a5cc8a05eb2c
ananyagoyal0624 <136187797+ananyagoyal0624@users.noreply.github.com>
2025-03-23 15:33:28 +0530

    Update qa_llm.py

--- qa_llm.py
+++ qa_llm.py
@@ -5,7 +5,7 @@ class QaLlm():
         self.llm = ChatOpenAI(
             temperature=0, 
             model_name="gpt-3.5-turbo",
-            openai_api_key="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"  # Yahan API key daalni hai
+            openai_api_key=""  # Yahan API key daalni hai
         )
 
     def get_llm(self):
